56f98580d324b8d11c5d3c1d26c2310fef6f94e6
imfarzadsh <farzaddd.shahbazi0010@gmail.com>
2024-11-25 20:49:28 +0330

    the first project

---
+++ A Simle Number Game/A-Simple-Number-Game-FirstSolution.py
@@ -0,0 +1,7 @@
+x = int(input('please enter your number between 1 and 9 :'))
+if 1 <= x <= 9 :
+    print( f' number : {((x * 2 + 8 + x - 2)/3 - x)* 4 }')
+    
+else :
+    print("*** input number is incorrect ***")
+    
